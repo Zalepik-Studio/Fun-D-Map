1032a86cc771baf3bc502312cd1b515304d8e146
Moh. Zainul Muttaqin | Zalepik <51511976+mzainulmuttaqin@users.noreply.github.com>
2023-03-28 07:12:48 +0700

    hampir

---
+++ javascript/js.js
@@ -0,0 +1,150 @@
+// const toastTrigger = document.getElementById('jawaban_benar_1_btn')
+// const toastLiveExample = document.getElementById('jawaban_benar_1')
+
+// if (toastTrigger) {
+// const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+// toastTrigger.addEventListener('click', () => {
+//     toastBootstrap.show()
+// })
+// }
+
+
+// const jawaban_salah = document.querySelectorAll('.jawaban_salah_btn');
+// const jawaban_salahtoastLiveExample = document.getElementById('jawaban_salah')
+
+// for (var i = 0; i < jawaban_salah.length; i++) {
+// if (jawaban_salah[i]) {
+//     const toastBootstrap = bootstrap.Toast.getOrCreateInstance(jawaban_salahtoastLiveExample)
+//     jawaban_salah[i].addEventListener('click', () => {
+//         toastBootstrap.show()
+
+//     })
+//     }
+// }
+// function myFunction() {
+//     document.getElementsByClassName('jawaban_no_1_a').disabled = true;
+//     document.getElementsByClassName('jawaban_no_1_b').disabled = true;
+//     document.getElementsByClassName('jawaban_no_1_c').disabled = true;
+//     document.getElementsByClassName('jawaban_no_1_d').disabled = true;
+// }
+
+function soal1Benar() {
+    document.getElementById("d-skor").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+    document.getElementById("d-skor_akhir_angka").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+}
+
+function soal1Lanjut() {
+    document.getElementById("d-soal_1").classList.add("d-hide");
+    document.getElementById("d-soal_2").classList.remove("d-hide");
+}
+
+//===
+function soal2Benar() {
+    document.getElementById("d-skor").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+    document.getElementById("d-skor_akhir_angka").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+}
+
+function soal2Lanjut() {
+    document.getElementById("d-soal_2").classList.add("d-hide");
+    document.getElementById("d-soal_3").classList.remove("d-hide");
+}
+
+//===
+function soal3Benar() {
+    document.getElementById("d-skor").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+    document.getElementById("d-skor_akhir_angka").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+}
+
+function soal3Lanjut() {
+    document.getElementById("d-soal_3").classList.add("d-hide");
+    document.getElementById("d-soal_4").classList.remove("d-hide");
+}
+
+//===
+function soal4Benar() {
+    document.getElementById("d-skor").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+    document.getElementById("d-skor_akhir_angka").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+}
+
+function soal4Lanjut() {
+    document.getElementById("d-soal_4").classList.add("d-hide");
+    document.getElementById("d-soal_5").classList.remove("d-hide");
+}
+
+//===
+function soal5Benar() {
+    document.getElementById("d-skor").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+    document.getElementById("d-skor_akhir_angka").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+}
+
+function soal5Lanjut() {
+    document.getElementById("d-soal_5").classList.add("d-hide");
+    document.getElementById("d-soal_6").classList.remove("d-hide");
+}
+
+//===
+function soal6Benar() {
+    document.getElementById("d-skor").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+    document.getElementById("d-skor_akhir_angka").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+}
+
+function soal6Lanjut() {
+    document.getElementById("d-soal_6").classList.add("d-hide");
+    document.getElementById("d-soal_7").classList.remove("d-hide");
+}
+
+//===
+function soal7Benar() {
+    document.getElementById("d-skor").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+    document.getElementById("d-skor_akhir_angka").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+}
+
+function soal7Lanjut() {
+    document.getElementById("d-soal_7").classList.add("d-hide");
+    document.getElementById("d-soal_8").classList.remove("d-hide");
+}
+
+//===
+function soal8Benar() {
+    document.getElementById("d-skor").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+    document.getElementById("d-skor_akhir_angka").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+}
+
+function soal8Lanjut() {
+    document.getElementById("d-soal_8").classList.add("d-hide");
+    document.getElementById("d-soal_9").classList.remove("d-hide");
+}
+
+//===
+function soal9Benar() {
+    document.getElementById("d-skor").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+    document.getElementById("d-skor_akhir_angka").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+}
+
+function soal9Lanjut() {
+    document.getElementById("d-soal_9").classList.add("d-hide");
+    document.getElementById("d-soal_10").classList.remove("d-hide");
+}
+
+//===
+function soal10Benar() {
+    document.getElementById("d-skor").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+    document.getElementById("d-skor_akhir_angka").innerHTML = parseInt(document.getElementById("d-skor").innerHTML) + 10;
+}
+
+function soal10Lanjut() {
+    document.getElementById("d-soal_dikerjakan").classList.add("d-hide");
+    document.getElementById("d-soal_10").classList.add("d-hide");
+    document.getElementById("d-skor_akhir").classList.remove("d-hide");
+    var d_skor = document.getElementById("d-skor_akhir_angka");
+    d_skor.innerHTML = document.getElementById("d-skor").innerHTML;
+    var d_status = document.getElementById("d-status_");
+    if (d_skor >= 70){
+        d_status.innerHTML = "Lulus";
+        d_status.classList.add("d-status_success")
+    }
+    else{
+        d_status.innerHTML = "Belum Lulus";
+        d_status.classList.add("d-status_gagal")
+    }
+}
